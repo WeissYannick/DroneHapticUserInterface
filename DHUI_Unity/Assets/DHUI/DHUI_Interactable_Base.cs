f440658dc4bce264ac786a4f4607f6a175328154
WeissYannick <yannick.weiss@gosheim.net>
2020-03-09 15:58:01 +0100

    Added Interactor and Interactabel scripts (still empty)

---
+++ DHUI_Unity/Assets/DHUI/DHUI_Interactable_Base.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DHUI.Core
+{
+    public class DHUI_Interactable_Base : MonoBehaviour
+    {
+        [SerializeField]
+        private float _primaryPriorityMultiplier = 1;
+    }
+}

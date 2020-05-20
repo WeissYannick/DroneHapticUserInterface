c4ddcdacc0ab266e72a89c70fb51860aaf80f24a
WeissYannick <yannick.weiss@gosheim.net>
2020-05-20 11:29:23 +0200

    Added TrailDrawing

---
+++ DHUI_Unity/Assets/DHUI/Diagnostics/DHUI_TrailDrawing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DHUI
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class DHUI_TrailDrawing : MonoBehaviour
+    {
+        private List<Vector3> positions = new List<Vector3>();
+        private LineRenderer lineRenderer = null;
+
+        private void Start()
+        {
+            lineRenderer = gameObject.GetComponent<LineRenderer>();
+            if (lineRenderer == null)
+            {
+                lineRenderer = gameObject.AddComponent<LineRenderer>();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (positions.Count > 0 && positions[positions.Count - 1] == transform.position) return;
+            
+            positions.Add(transform.position);
+            RenderTrail();
+        }
+
+        protected void RenderTrail()
+        {
+            if (positions.Count < 2)
+            {
+                lineRenderer.enabled = false;
+            }
+            else
+            {
+                lineRenderer.enabled = true;
+            }
+            lineRenderer.positionCount = positions.Count;
+            lineRenderer.SetPositions(positions.ToArray());
+        }
+    }
+
+}

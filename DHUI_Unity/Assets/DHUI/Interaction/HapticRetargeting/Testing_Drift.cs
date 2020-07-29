ad242b737ccada7107bd3455386aa6052e309998
WeissYannick <yannick.weiss@gosheim.net>
2020-07-29 18:10:21 +0200

    Added sim. Drift to test haptic retargeting

---
+++ DHUI_Unity/Assets/DHUI/Interaction/HapticRetargeting/Testing_Drift.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing_Drift : MonoBehaviour
+{
+    Vector3 currentDriftVector = Vector3.zero;
+
+    private float lastTimeUpdated = 0;
+    private float updateInterval = 3;
+
+    void Update()
+    {
+        if (Time.time > lastTimeUpdated + updateInterval)
+        {
+            lastTimeUpdated = Time.time;
+
+            currentDriftVector = new Vector3(Random.value - 0.5f, Random.value  - 0.5f, Random.value - 0.5f);
+        }
+
+        transform.Translate(currentDriftVector * 0.0003f);
+    }
+}

bf522da60774c92ef1c78bd8cf03abda941714d4
WeissYannick <yannick.weiss@gosheim.net>
2020-10-13 17:00:58 +0200

    Added Blank Script for future EmergencyInput via Vive Controllers

---
+++ DHUI_Unity/Assets/DHUI/UserInput/EmergencyInput/DHUI_EmergencyInput_ViveController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace DHUI.Core
+{
+    // <summary>
+    /// This class handles emergency inputs by Vive Controllers.
+    /// </summary>
+    public class DHUI_EmergencyInput_ViveController : DHUI_EmergencyInput_Base
+    {
+        // TODO
+        
+    }
+}
